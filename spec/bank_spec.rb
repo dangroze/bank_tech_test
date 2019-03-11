ad991c35ba07c061ae8372d534af64836c4296a0
Dan Groze <dangroze@gmail.com>
2019-03-11 22:12:01 +0000

    :white_check_mark::recycle:Test- can withdraw money + Refactor tests

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,10 +2,17 @@ require 'bank'
 RSpec.describe Bank do
   subject(:bank) {described_class.new}
 
-  describe('Can add credit to balance') do
-    it('Should have a method to add credit') do
-      bank.add_credit(1000)
+  context('When depositing money') do
+    it('Increases balance') do
+      bank.deposit(1000)
       expect(subject.balance).to eq(1000)
+    end
+  end
+  context('When withdrawing money') do
+    it('Decreases balance') do
+      bank.deposit(1000)
+      bank.withdraw(500)
+      expect(bank.balance).to eq(500)
     end
   end
 end
