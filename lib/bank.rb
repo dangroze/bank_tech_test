9c115038493175c74c4178a946321abe7033871e
Dan Groze <dangroze@gmail.com>
2019-03-12 13:08:29 +0000

    :sparkles::recycle:Passed test- records details of transaction on deposit

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,13 +1,18 @@
+require_relative 'date_format'
 class Bank
-  attr_reader :balance
+  include DateFormat
+
+  attr_reader :balance, :transactions
   DEFAULT_BALANCE = 0;
   
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @transactions = []
   end
 
   def deposit(amount)
     @balance += amount
+    @transactions.push([date, amount, " ", @balance])
   end
 
   def withdraw(amount)
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,18 +1,28 @@
 require 'bank'
 RSpec.describe Bank do
   subject(:bank) {described_class.new}
+  let(:an_amount) { 1000 }
+  let(:a_date) { '12/03/2019' }
+  let(:type) { ' ' }
 
   context('When depositing money') do
     it('Increases balance') do
-      bank.deposit(1000)
-      expect(subject.balance).to eq(1000)
+      bank.deposit(an_amount)
+      expect(subject.balance).to eq(an_amount)
     end
   end
   context('When withdrawing money') do
     it('Decreases balance') do
-      bank.deposit(1000)
-      bank.withdraw(500)
-      expect(bank.balance).to eq(500)
+      bank.deposit(an_amount)
+      bank.withdraw(an_amount)
+      expect(bank.balance).to eq(0)
+    end
+  end
+  context('transaction details') do
+    it('records date, amount, debit and balance on deposit') do
+      allow(bank).to receive(:date).and_return(a_date)
+      bank.deposit(an_amount)
+      expect(bank.transactions).to eq([[a_date, an_amount, type, bank.balance]])
     end
   end
 end
