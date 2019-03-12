634466593181509fa57873e4b1c7d673e1812f66
Dan Groze <dangroze@gmail.com>
2019-03-12 13:06:47 +0000

    :sparkles:Add date formatter module

---
+++ lib/date_format.rb
@@ -0,0 +1,14 @@
+module DateFormat
+
+  def date
+    arr = [].push(Time.new)
+    time = arr.join('').slice!(0, 10).gsub!('-', '/')
+    order(time)
+  end
+
+  def order(time)
+    ordered = [].push(time.slice!(8, 10)).push(time.slice!(4, 5)).push(time.slice!(0, 4))
+    ordered.join('')
+  end
+
+end
