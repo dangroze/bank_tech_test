d73dd23e4c5f90a24e35b934c524e711232e52bf
dangroze <dangroze@gmail.com>
2019-04-18 16:45:06 +0100

    Fixed Statement test

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe Statement do
   subject(:statement) { described_class.new(bank.transactions) }
   
   it('Prints the formatted statement') do
-    statement.print(bank.transactions)
-    expect output('date || credit || debit || balance
-    12/03/2019 || 1000 ||  || 1000').to_stdout
+    # statement.print(bank.transactions)
+
+    expect { statement.print(bank.transactions) }.to output('date || credit || debit || balance
+12/03/2019 || 1000 ||   || 1000
+').to_stdout
   end
 end
